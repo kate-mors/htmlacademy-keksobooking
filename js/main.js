cc0736fc130f8628b4477badab1100274726933b
Ekaterina <ekaterina.morsino@gmail.com>
2021-02-04 14:31:47 +0100

    Beautifying main

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 'use strict';
 
-const getRandom = function (min, max) {
+const getRandom = function(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   if (min >= 0 && max >= 0) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  } else {return false;
+  } else {
+    return false;
   }
 }
 
 getRandom(10.09, 0.09);
 
 
-const getRandomFloat = function (min, max, floatLength) {
+const getRandomFloat = function(min, max, floatLength) {
 
   if (min >= 0 && max >= 0 && floatLength >= 0) {
     return +(Math.random() * (max - min + 1) + min).toFixed(floatLength);
-  } else {return false;
+  } else {
+    return false;
   }
 }
 
