9126fd707bc7e3321384fc21b3f42693c33fb3c7
Ekaterina Ponomareva <70647825+kate-mors@users.noreply.github.com>
2021-02-18 12:03:27 +0100

    Отрисуй меня полностью (часть 1) (#5)
    * Installing ESlint
    
    * Создаю модули
    
    * Создаю новые карточти по образцу
    
    * Распределяю константы и функции по модулям
    
    * Меняю название переменной

--- js/util.js
+++ js/util.js
@@ -36,10 +36,22 @@ const shuffleArray = function(array) {
   return array;
 };
 
+const generateWordsEnding = function (number, array) {
+  if (number === 1) {
+    return array[0];
+  } if (number > 1 && number < 5) {
+    return array [1];
+  }
+  if (number === 0 || number >=5) {
+    return array[2];
+  }
+}
+
 export {
   getRandomIntInclusive,
   getRandomFloatInclusive,
   generateRandomValue,
   generateRandomList,
-  shuffleArray
+  shuffleArray,
+  generateWordsEnding
 };
