025399cd985e3c94fa423fb4e93d7bd18fcdfa7a
Ekaterina <ekaterina.morsino@gmail.com>
2021-03-23 18:35:55 +0100

    Добавляю картинки

--- js/avatar.js
+++ js/avatar.js
@@ -2,7 +2,9 @@ const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const avatarInput = document.querySelector('.ad-form__field input[type=file]');
 const avatarPreview = document.querySelector('.ad-form-header__avatar');
-
+const adPhoto = document.querySelector('.ad-form__photo');
+const adPhotoInput = document.querySelector('.ad-form__upload input[type=file]');
+const adPhotoPreviewTemplate = document.querySelector('#ad-photo__template').content.querySelector('.ad-photo__preview');
 
 const appendImage = function (fileChooser, preview) {
   const file = fileChooser.files[0];
@@ -18,7 +20,6 @@ const appendImage = function (fileChooser, preview) {
     reader.addEventListener('load', () => {
       preview.src = reader.result;
     });
-
     reader.readAsDataURL(file);
   }
 };
@@ -27,4 +28,12 @@ const avatarChangeHandler = function () {
   appendImage(avatarInput, avatarPreview);
 };
 
+const imageUploadHandler = function () {
+  const adPhotoPreview = adPhotoPreviewTemplate.cloneNode(true);
+
+  appendImage(adPhotoInput, adPhotoPreview);
+  adPhoto.appendChild(adPhotoPreview);
+}
+
 avatarInput.addEventListener('change', avatarChangeHandler);
+adPhotoInput.addEventListener('change', imageUploadHandler);
